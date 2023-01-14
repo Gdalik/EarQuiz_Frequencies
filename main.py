ff01493675e9fc4efbb293fd4385d3b246c8cce2
Gdalik <gdalik@gmail.com>
2023-01-14 05:57:46 +0300

    14.01.2023 Mac

--- main.py
+++ main.py
@@ -7,6 +7,6 @@ import sys
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    mw = MainWindow()
     app.setWindowIcon(QIcon(":/App/Icons/Logo/EM_Logo.png"))
+    mw = MainWindow()
     app.exec()
