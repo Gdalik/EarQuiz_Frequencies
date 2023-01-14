911e53828a81026f855da8c4ad16d28312a27af5
Gdalik <gdalik@gmail.com>
2023-01-14 15:20:05 +0300

    14.01.2023 Mac

--- Model/AudioEngine/process.py
+++ Model/AudioEngine/process.py
@@ -3,7 +3,7 @@ from Misc.calc import proc_unproc_len, rand_buffer
 import numpy as np
 
 
-def eq_proc(cur_sample, samplerate: int, freq: int or float, freq2=None, boost_cut='+', boost_cut2='+', gain_depth=12, Q=1.41, proc_t_perc=40):
+def eq_proc(cur_sample, samplerate: int, freq1: int or float, freq2=None, boost_cut1='+', boost_cut2='+', gain_depth=12, Q=1.41, proc_t_perc=40):
     def sec2fr(sec: int or float):
         return int(samplerate * sec)
 
@@ -14,8 +14,8 @@ def eq_proc(cur_sample, samplerate: int, freq: int or float, freq2=None, boost_c
         return _equalized
     cur_sample_length = cur_sample[0].size / samplerate
     proc_len, unproc_len = proc_unproc_len(cur_sample_length, proc_t_perc)
-    gain = gain_depth*-1 if boost_cut == '-' else gain_depth
-    eq1 = PeakFilter(cutoff_frequency_hz=freq, gain_db=gain, q=Q)
+    gain = gain_depth*-1 if boost_cut1 == '-' else gain_depth
+    eq1 = PeakFilter(cutoff_frequency_hz=freq1, gain_db=gain, q=Q)
     if freq2:
         gain2 = gain_depth*-1 if boost_cut2 == '-' else gain_depth
         eq2 = PeakFilter(cutoff_frequency_hz=freq2, gain_db=gain2, q=Q)
