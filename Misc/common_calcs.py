f38a1adaaffeb700e6537a944454d22cbfef09ea
Gdalik <gdalik@gmail.com>
2023-01-17 15:21:51 +0300

    17.01.2023 Mac

--- Misc/common_calcs.py
+++ Misc/common_calcs.py
@@ -1,5 +1,5 @@
-def findAdjacentEl(l: list, element, num=1):
-    element_ind = l.index(element)
+def findAdjacentEl(L: list, element, num=1):    # num: the maximum number of adjacent elements from each side
+    element_ind = L.index(element)
     min_ind = max(0, element_ind - num)
-    max_ind = min(len(l)-1, element_ind+num)
-    return [l[i] for i in range(min_ind, max_ind + 1) if l[i] != element]
+    max_ind = min(len(L) - 1, element_ind + num)
+    return [L[i] for i in range(min_ind, max_ind + 1) if L[i] != element]
