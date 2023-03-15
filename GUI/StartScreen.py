c992327c1fadaa4cdca93772e3f4f5864d0ee7d0
Gdaliy <gdalik@gmail.com>
2023-03-16 01:30:35 +0300

    16.03.2023 Win

--- GUI/StartScreen.py
+++ GUI/StartScreen.py
@@ -1,5 +1,6 @@
 from PyQt6.QtWidgets import QSplashScreen
-from PyQt6.QtGui import QPixmap
+from PyQt6.QtGui import QPixmap, QColor
+from PyQt6.QtCore import Qt
 
 
 class StartScreen(QSplashScreen):
@@ -7,3 +8,4 @@ class StartScreen(QSplashScreen):
         super().__init__()
         logo = QPixmap("icons:/Logo/EarQuiz_Splash.png")
         self.setPixmap(logo)
+        self.showMessage('Version 0.3.0', Qt.AlignmentFlag.AlignCenter, color=QColor('black'))
