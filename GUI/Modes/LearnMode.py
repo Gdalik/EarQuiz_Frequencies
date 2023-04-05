7833bb934e1ec63a733f75a178b55b1ca499ff0f
Gdalik <gdalik@gmail.com>
2023-04-05 07:01:29 +0300

    04.04.2023 Mac

--- GUI/Modes/LearnMode.py
+++ GUI/Modes/LearnMode.py
@@ -54,6 +54,7 @@ class LearnMode(UniMode):
     def playbackStoppedEnded(self):
         self.parent.EQContr.resetEQ()
         self.view.EQSetView.setEnabled(True)
+        self.view.EqOnOffLab.setVisible(False)
 
     def playbackEnded(self):
         if self.parent.mw_view.actionSequential_Playback.isChecked():
