ce279ca67ec44956e5768d168425ba17d0e46962
Gdaliy Garmiza <gdalik@gmail.com>
2025-02-06 02:02:03 +0300

    Environment variables, runtime hook for PyInstaller

--- env_vars.py
+++ env_vars.py
@@ -2,4 +2,5 @@ import os
 
 
 os.environ['QT_MEDIA_BACKEND'] = 'ffmpeg'
-os.environ['QT_LOGGING_RULES'] = 'qt.multimedia.ffmpeg=false'
+os.environ['QT_LOGGING_RULES'] = '*.multimedia.*=false'
+os.environ['QT_FFMPEG_DECODING_HW_DEVICE_TYPES'] = ','
