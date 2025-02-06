fd731f9bcde02b432afb9737c98978f3a00ae2d6
Gdaliy Garmiza <gdalik@gmail.com>
2025-02-06 17:03:07 +0300

    No ffmpeg hardware encoding

--- env_vars.py
+++ env_vars.py
@@ -4,3 +4,4 @@ import os
 os.environ['QT_MEDIA_BACKEND'] = 'ffmpeg'
 os.environ['QT_LOGGING_RULES'] = '*.multimedia.*=false'
 os.environ['QT_FFMPEG_DECODING_HW_DEVICE_TYPES'] = ','
+os.environ['QT_FFMPEG_ENCODING_HW_DEVICE_TYPES'] = ','
